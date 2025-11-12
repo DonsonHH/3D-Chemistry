9b376041d6dd5283ec854b543b9c9b636b9c309d
ypx <donsonho@qq.com>
2025-11-12 20:42:38 +0800

    Initial Commit

---
+++ 3D-Chemistry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3D-Chemistry
+//
+//  Created by ypx on 2025/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3D-Chemistry/_D_ChemistryApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_ChemistryApp.swift
+//  3D-Chemistry
+//
+//  Created by ypx on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct _D_ChemistryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
